a55afb166680ec1571d64fa209ed601b547c9bb0
rapid0old <kazsa.seko@gmail.com>
2026-08-14 23:22:40 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,25 @@
-const CACHE = 'tigers-probability-v0.2';
-const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+const CACHE='tigers-probability-v0.3';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));
   self.skipWaiting();
 });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(
-    caches.match(event.request).then(cached =>
-      cached || fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        return response;
-      }).catch(() => caches.match('./index.html'))
-    )
-  );
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  if(event.request.mode==='navigate'){
+    event.respondWith(
+      fetch(event.request).then(r=>{
+        const copy=r.clone(); caches.open(CACHE).then(c=>c.put('./index.html',copy)); return r;
+      }).catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(r=>{
+    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(event.request,copy)); return r;
+  })));
 });
