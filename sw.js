d56131e0fb32cd3c10c0cae678b2fb02507e58ac
rapid0old <kazsa.seko@gmail.com>
2026-08-23 19:53:33 +0900

    Release v0.5 trial improvements

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tigers-probability-v0.4';
+const CACHE='tigers-probability-v0.5-trial';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{
