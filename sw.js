b75b03e62c3f59deaa1f56967ecfc5607f7d72d8
rapid0old <kazsa.seko@gmail.com>
2026-08-23 21:19:24 +0900

    Release v0.8 saved opponent lineups
    Save opponent starting lineups, support both teams' substitutions, add sacrifice bunts, and collapse player settings.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tigers-probability-v0.7-trial';
+const CACHE='tigers-probability-v0.8-trial';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{
