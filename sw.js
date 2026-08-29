2fdc095b61b65a825bbd8e3efa41ded5bbe54fb3
rapid0old <kazsa.seko@gmail.com>
2026-08-29 12:27:34 +0900

    Update sw.js
    2.0.1→2.0.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tigers-probability-v2.0.1-trial';
+const CACHE='tigers-probability-v2.0.2-trial';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg','./v10.css','./rosters.js','./stats-v13.js','./app-v10.js'];
 
 self.addEventListener('install',event=>{
